b5b7756d76b223423718c5572e150446370cb34f
conf8o <41929738+conf8o@users.noreply.github.com>
2024-05-11 17:34:06 +0900

    add obs client and adjust types

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,52 @@
 extern crate multiinput;
 
 use multiinput::*;
+use anyhow::Result;
 mod router;
+use serde::Deserialize;
+use obws::Client;
+use std::fs;
+use toml;
 
+#[derive(Debug, Deserialize)]
+pub struct ObsClientConfig {
+    host: Option<String>,
+    port: Option<u16>,
+    password: Option<String>
+}
+
+pub async fn init_client_from_config(path: &str) -> Result<Client> {
+    let s = fs::read_to_string(path)?;
+    let config: ObsClientConfig = toml::from_str(s.as_str())?;
+    match config {
+        ObsClientConfig { host: Some(host), port: Some(port), password } => {
+            Result::Ok(Client::connect(host, port, password).await?)
+        }
+
+        _ => {
+            Result::Err(anyhow::anyhow!("Invalid config."))
+        }
+            
+    }
+}
 
 const PRESS_W: usize = 1;
 
-fn init_router() -> router::Router {
+fn init_router(obs_client: &Client) -> router::Router {
     let router  = router::Router::init(
         vec![
-            (PRESS_W, || 1)
+            (PRESS_W, router::no_process)
         ]
     );
 
     router
 }
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<()> {
+
+    let client = init_client_from_config("./obs_connetion.toml").await?;
+
     let mut manager = RawInputManager::new().unwrap();
     manager.register_devices(DeviceType::Joysticks(XInputInclude::True));
     manager.register_devices(DeviceType::Keyboards);
@@ -32,4 +62,5 @@ fn main() {
         }
     }
     println!("Finishing");
+    Ok(())
 }
--- src/router.rs
+++ src/router.rs
@@ -1,22 +1,31 @@
+use anyhow::Result;
+use obws::Client;
+
 type ShortCutIndex = usize;
-type Process = fn () -> i32;
+type Process = fn (&Client) -> Result<()>;
+
+
 pub struct Router {
     pub handlers: Vec<Process>
 }
 
 impl Router {
-    fn handle(&self, index: ShortCutIndex) -> i32 {
+    fn handle(&self, client: &Client, index: ShortCutIndex) -> Result<()>  {
         let handle = &self.handlers[index];
-        handle()
+        handle(client)
     }
 
-    pub fn init(handler_pairs: Vec<(ShortCutIndex, fn () -> i32)>) -> Router {
+    pub fn init(handler_pairs: Vec<(ShortCutIndex, fn (&Client) -> Result<()>)>) -> Router {
         let max_index = *handler_pairs.iter().map( |(i, _)| i ).max().unwrap();
-        let mut handlers: Vec<Process> = vec![|| 0; max_index];
+        let mut handlers: Vec<Process> = vec![no_process; max_index];
         for (index, handler) in &handler_pairs {
             handlers[*index] = *handler;
         }
 
-        Router { handlers: handlers}
+        Router { handlers: handlers }
     }
+}
+
+pub fn no_process(_ : &Client) -> Result<()> {
+    Ok(())
 }
